6b7b1c6b37944a46396e4cdc3d28770959701f6e
Deivison Lourenço <142614578+Ridd1kulusC0d3r@users.noreply.github.com>
2023-08-30 23:48:16 -0300

    Create buscanamefb.js

---
+++ Testes/Java_script/buscanamefb.js
@@ -0,0 +1,8 @@
+Aqui está um exemplo simples de como você pode usar JavaScript para realizar uma busca de perfil público do Facebook (lembre-se de que isso deve ser feito de maneira ética e legal):
+
+// Exemplo de busca de perfil público no Facebook
+var username = "nome-de-usuario"; // Substitua pelo nome de usuário desejado
+var url = "https://www.facebook.com/" + username;
+
+// Abre o perfil no navegador
+window.open(url, "_blank");
